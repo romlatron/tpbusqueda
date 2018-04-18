12bbbb88ef70d20c827222fa49ce210b72da9863
V1nkey <kevin.chidiac@gmail.com>
2018-04-18 18:55:16 -0300

    Beginning Project

---
+++ searchAlgorithms/src/searchalgorithms/SearchAlgorithms.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searchalgorithms;
+
+/**
+ *
+ * @author Kevin
+ */
+public class SearchAlgorithms 
+{
+    public int depthFirst(Node root, Node objective)
+    {
+        if (root == objective)
+            return 1;
+        
+        for 
+    }
+    
+    public void breadthFirst()
+    {
+        
+    }
+    
+    
+}
