1da40b3e69585707175215c6392ea5b5c3f0bcc0
V1nkey <kevin.chidiac@gmail.com>
2018-04-20 16:36:52 -0300

    test

--- searchAlgorithms/src/searchalgorithms/SearchAlgorithms.java
+++ searchAlgorithms/src/searchalgorithms/SearchAlgorithms.java
@@ -4,25 +4,44 @@
  * and open the template in the editor.
  */
 package searchalgorithms;
+import ar.com.itba.sia.Problem;
+import ar.com.itba.sia.Rule;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 
 /**
  *
  * @author Kevin
  */
-public class SearchAlgorithms 
+public class SearchAlgorithms
 {
-    public int depthFirst(Node root, Node objective)
+    public void depthFirst(Problem p, Object currentState, boolean isResolved)
     {
-        if (root == objective)
-            return 1;
+        isResolved = p.isResolved(currentState);
+        if (isResolved)
+            return;
         
-        for 
+        List<Rule<Object>>rules = p.getRules(currentState);
+        for (Rule<Object> rule : rules)
+            depthFirst(p, rule.applyToState(currentState), isResolved);
     }
     
-    public void breadthFirst()
+    public void breadthFirst(Problem p)
     {
+        Queue<Object> queue = new LinkedList<Object>();
+        queue.add(p.getInitialState());
         
+        while(!queue.isEmpty() && !p.isResolved(queue.element()))
+        {   
+            Object tmpObj = queue.poll();
+            System.out.println(tmpObj);
+            
+            List<Rule<Object>>rules = p.getRules(tmpObj);
+            for (Rule<Object> rule : rules)
+                queue.add(rule.applyToState(tmpObj));   
+        }
     }
-    
     
 }
